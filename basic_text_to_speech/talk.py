7f79208ef5d1e3209792449011a0fcfb42343d64
talksik <patel.arjun@gmail.com>
2023-04-27 16:47:46 -0700

    printing command

--- basic_text_to_speech/talk.py
+++ basic_text_to_speech/talk.py
@@ -4,14 +4,17 @@ import os
 
 
 def robot(text):
-    os.system('espeak --stdin "' + text + '" | aplay -D plughw:2,0 ')
+    command = 'espeak --stdin "' + text + '" | aplay -D plughw:2,0 '
+    os.system(command)
+    return command
 
 
 if __name__ == '__main__':
     while True:
 
         try:
-            robot("Hello world how are you doing today?")
+            command = robot("Hello world how are you doing today?")
+            print(command)
             time.sleep(5)
         except KeyboardInterrupt:
             break
