ef04b2382767047734f06bab990fbd77c109b958
talksik <patel.arjun@gmail.com>
2023-04-27 17:02:34 -0700

    simplifying

--- basic_text_to_speech/talk.py
+++ basic_text_to_speech/talk.py
@@ -1,15 +1,11 @@
 import time
 
-from subprocess import Popen, PIPE
+import os
 
 
 def robot(text):
     print("executing command")
-    espeak = Popen(
-        ['espeak', '--stdin "Hello world how are you doing today?"'], stdout=PIPE)
-    aplay = Popen(['aplay', '-D plughw:2,0'], stdin=espeak.stdout, stdout=PIPE)
-    espeak.stdout.close()  # enable write error in dd if ssh dies
-    out, err = aplay.communicate()
+    os.system("espeak -w output.wav \"" + text + "\"")
 
 
 if __name__ == '__main__':
